8bee550f8c7154f81a74d6f9ce60d4c8128e08e8
shuribuzz <35268736+shuribuzz@users.noreply.github.com>
2018-05-04 10:35:30 +0500

    Update CheatActivity.java

--- app/src/main/java/ru/alexandrpokh/geoquiz/CheatActivity.java
+++ app/src/main/java/ru/alexandrpokh/geoquiz/CheatActivity.java
@@ -13,7 +13,7 @@ public class CheatActivity extends AppCompatActivity {
     public static final String EXTRA_ANSWER_SHOWN = "ru.alexandrpokh.geoquiz.answer_shown";
     private  static final String KEY_IS_CHEAT = "cheat";
     private boolean mAnswerIsTrue;
-  //  private boolean mIsCheater;
+    private boolean mIsCheater=false;
     private TextView mAnswerTextView;
     private Button mShowAnswer;
 
@@ -21,11 +21,13 @@ public class CheatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (savedInstanceState != null) {
-            System.out.println(mAnswerIsTrue);
-            mAnswerIsTrue = savedInstanceState.getBoolean(KEY_IS_CHEAT);
+            System.out.println(mIsCheater);
+            mIsCheater = savedInstanceState.getBoolean(KEY_IS_CHEAT, false);
         }
         setContentView(R.layout.activity_cheat);
-     //   mIsCheater = false;
+        
+        setAnswerShownResult(mIsCheater);
+
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
 
         mAnswerTextView = (TextView)findViewById(R.id.answerTextView);
@@ -41,7 +43,7 @@ public class CheatActivity extends AppCompatActivity {
                 } else {
                     mAnswerTextView.setText(R.string.false_button);
                 }
-             //   mIsCheater = true;
+            
 
                 setAnswerShownResult(true);
             }
@@ -52,13 +54,14 @@ public class CheatActivity extends AppCompatActivity {
         Intent dataCheat = new Intent();
         dataCheat.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
         setResult(RESULT_OK, dataCheat);
+        mIsCheater = isAnswerShown;
     }
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
-        System.out.println(mAnswerIsTrue + "sav");
-        savedInstanceState.putBoolean(KEY_IS_CHEAT, mAnswerIsTrue);
+        System.out.println(mIsCheater + " - saved");
+        savedInstanceState.putBoolean(KEY_IS_CHEAT, mIsCheater);
     }
 
 }
